7e6b47f1e046cd959d8c9082fb30398bb17874b2
edmondgaks <edmondgaks@gmail.com>
2024-03-08 12:05:14 +0200

    Building homescreen: Screenheader buttons

--- app/index.js
+++ app/index.js
@@ -1,10 +1,34 @@
-import { View, Text } from "react-native";
+import { View, Text, ScrollView, SafeAreaView } from "react-native";
+import { COLORS, icons, images, SIZES } from "../constants";
+import { Nearbyjobs, Popularjobs, ScreenHeaderBtn, Welcome } from "../components";
+import { Stack, useRouter } from "expo-router";
 
 const Home = () => {
+    const router = useRouter();
     return (
-        <View>
-            <Text>Home</Text>
-        </View>
+        <ScrollView style={{ flex: 1, backgroundColor: COLORS.lightWhite}}>
+            <Stack.Screen options={{ 
+                headerStyle: {backgroundColor: COLORS.lightWhite},
+                headerShadowVisible: false,
+                headerLeft: () => (
+                    <ScreenHeaderBtn iconUrl={icons.menu} dimension="60%" />
+                ),
+                headerRight: () => (
+                    <ScreenHeaderBtn iconUrl={icons.profile} dimension="100%" />
+                ),
+                headerTitle: ""
+            }} />
+            <ScrollView showsVerticalScrollIndicator={false}>
+                <View style={{
+                    flex: 1,
+                    padding: SIZES.medium
+                }}>
+                    <Welcome />
+                    <Popularjobs />
+                    <Nearbyjobs />
+                </View>
+            </ScrollView>
+        </ScrollView>
     )
 }
 

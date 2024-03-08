eba823984477cd2aa531fbd4e4304899266b9f42
edmondgaks <edmondgaks@gmail.com>
2024-03-08 21:57:51 +0200

    Adding the popular jobs part in the Welcome page

--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -13,8 +13,15 @@ import { COLORS, SIZES } from "../../../constants";
 import PopularJobCard from "../../common/cards/popular/PopularJobCard";
 
 const Popularjobs = () => {
+  const router = useRouter();
   const isLoading = false;
   const error = false;
+  const [selectedJob, setSelectedJob] = useState();
+
+  const handleCardPress = (item) => {
+    router.push(`/job-details/${item.job_id}`);
+    setSelectedJob(item.job_id);
+  };
   return (
     <View style={styles.container}>
       <View style={styles.header}>
@@ -34,6 +41,8 @@ const Popularjobs = () => {
             renderItem={({ item }) => (
               <PopularJobCard
                   item={item}
+                  selectedJob={selectedJob}
+                  handleCardPress={handleCardPress}
                 />
               )} 
             keyExtractor={(item) => item.job_id}
